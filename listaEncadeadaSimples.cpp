7e2eb423c237fbef98a454b7dec2e6ba956a32ce
SuitingPie52 <126472855+SuitingPie52@users.noreply.github.com>
2023-08-08 09:31:02 -0300

    Add files via upload

---
+++ listaEncadeadaSimples.cpp
@@ -0,0 +1,89 @@
+//////////////////////////////////////////
+// PROGRAMA DE LISTA ENCADEADA SIMPLES //
+////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/////////////////////////////////////////////////////////////////////
+// em ponteiros você guarda no endereço outro endereço de memória //
+// *p ---> declara ponteiro                                      //
+// & ---> endereça a variável                                   //
+/////////////////////////////////////////////////////////////////
+
+///////////////////////////////////
+// Definicao da Estrutura do No //
+/////////////////////////////////
+
+struct no {
+	
+	int numero;
+	struct no *proximo;
+	
+};
+
+struct no *cabeca;
+
+///////////////////////////////////////
+// Funcao que insere um No na Lista //
+/////////////////////////////////////
+
+void inserir (int parametroNumero) {
+	
+	struct no *novoNo = NULL;
+	novoNo = (struct no *) malloc(sizeof(struct no *)); // malloc == memory allocation, do tamanho de um "struct no"
+	novoNo -> numero = parametroNumero;
+	novoNo -> proximo = NULL;
+	
+	if (cabeca == NULL) {
+		
+		cabeca = novoNo;
+		
+	} else {
+		
+		struct no *ponteiro = cabeca;
+		
+		while (ponteiro -> proximo != NULL) {
+			
+			ponteiro = ponteiro -> proximo;
+			
+		}
+		
+		ponteiro -> proximo = novoNo;
+		
+	}
+	
+}
+
+void imprimir () {
+	
+	struct no *ponteiro = cabeca;
+	
+	while (ponteiro != NULL) {
+		
+		printf("%d\n", ponteiro -> numero);
+		ponteiro = ponteiro -> proximo;
+		
+	}
+	
+}
+
+int main () {
+	
+	inserir(1);
+	inserir(2);
+	inserir(3);
+	inserir(4);
+	inserir(5);
+	inserir(6);
+	inserir(7);
+	inserir(8);
+	inserir(9);
+	inserir(10);
+	inserir(11);
+	
+	imprimir();
+	
+	return 0;
+	
+}
